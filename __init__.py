dce2b57538f0cf7e9aaf668ca94b9d504de55533
xtreme byte <mehrullosanginovw@gmail.com>
2025-02-27 20:01:50 +1000

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ bl_info = {
     "blender": (2, 80, 0),
     "category": "Import-Export",
     "author": "xtreme byte",
-    "version": (3, 0, 1),
+    "version": (3, 1, 1),
     "location": "View3D > N-Panel > GTA SA IPL",
     "description": "Imports GTA SA IPL files with correct coordinates.",
 }
