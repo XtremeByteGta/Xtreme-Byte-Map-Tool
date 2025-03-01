394769a29da602c2c81b38a2ca862b8eb898b232
xtreme byte <mehrullosanginovw@gmail.com>
2025-03-01 18:39:38 +1000

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,15 +1,37 @@
+# MIT License
+#
+# Copyright (c) 2025 xtreme byte
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 bl_info = {
     "name": "xtreme byte MAP Tool",
     "blender": (2, 80, 0),
     "category": "Import-Export",
     "author": "xtreme byte",
-    "version": (1, 1, 0),
-    "location": "View3D > N-Panel > Xtreme Byte",
+    "version": (1, 2, 0),
+    "location": "View3D > N-Panel > GTA SA IPL",
     "description": "Imports GTA SA IPL files with correct coordinates.",
 }
 
-from .gta_sa_ipl_importer import register, unregister
+from .gui import register, unregister
 
 if "bpy" in locals():
     import importlib
-    importlib.reload(gta_sa_ipl_importer)
+    importlib.reload(gui)
